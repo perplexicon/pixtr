e291382d0b69c4dff70288089f59d76803a0e2fe
perplexicon <alexgarkavenko@gmail.com>
2014-09-17 21:17:27 +1200

    Initial Commit

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,29 @@
+class GalleriesController < ApplicationController
+  def index
+    @galleries = Gallery.all
+  end
+  def destroy
+    gallery = Gallery.find(params[:id])
+    gallery.destroy
+    redirect_to "/galleries"
+  end
+  def new
+    @gallery = Gallery.new
+  end
+  def create
+    Gallery.create(gallery_params)
+    redirect_to "/galleries"
+  end
+
+  def show
+    @gallery = Gallery.find(params[:id])
+  end
+
+  private
+
+  def gallery_params
+    params.
+      require(:gallery).
+      permit(:name, :description)
+  end
+end
