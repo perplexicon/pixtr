279bb1043f7e021823605a2f037e5b42cdf082a4
perplexicon <alexgarkavenko@gmail.com>
2014-09-17 21:30:01 +1200

    User can edit and delete galleries
    * Update galleries#show to include "edit" link
    * Add edit route
    * Add edit and update actions in galleries_controller
    * Make edit view

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -5,20 +5,29 @@ class GalleriesController < ApplicationController
   def destroy
     gallery = Gallery.find(params[:id])
     gallery.destroy
-    redirect_to "/galleries"
+    redirect_to galleries_path
   end
   def new
     @gallery = Gallery.new
   end
   def create
     Gallery.create(gallery_params)
-    redirect_to "/galleries"
+    redirect_to galleries_path
   end
 
   def show
     @gallery = Gallery.find(params[:id])
   end
 
+  def edit
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def update
+    gallery = Gallery.find(params[:id])
+    gallery.update(gallery_params)
+    redirect_to gallery_path(gallery)
+  end
   private
 
   def gallery_params
